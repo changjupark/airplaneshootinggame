cf779d7b7dd51f7f39fdc969eb6835ae03847d6e
kwanpil <95747714+kwanpil@users.noreply.github.com>
2022-11-14 02:21:26 +0900

    엔진 스크립트

---
+++ script/Enemy.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int              damage = 1;
+    [SerializeField]
+    private int              scorePoint = 100;
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("Player") )
+        {
+            
+            collision.GetComponent<PlayerHP>(). TakeDamage(damage);
+
+            OnDie();
+        }
+    }
+
+    public void OnDie()
+    {
+        playerController.Score += scorePoint;
+        Destroy(gameObject);
+    }
+}
---
+++ script/Projectile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("Enemy") )
+        {
+            collision.GetComponent<Enemy>().OnDie();
+            Destroy(gameObject);
+        }
+    }
+}
